679e90ce310db4ad5a844ee775c08c85959874c4
Nathan Brueck <nate.brueck@gmail.com>
2024-03-08 19:16:12 -0700

    Added to Index.js

--- index.js
+++ index.js
@@ -33,3 +33,25 @@ app.use((_req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+let scores = [];
+function updateScores(newScore, scores) {
+  let found = false;
+  for (const [i, prevScore] of scores.entries()) {
+    if (newScore.score > prevScore.score) {
+      scores.splice(i, 0, newScore);
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    scores.push(newScore);
+  }
+
+  if (scores.length > 10) {
+    scores.length = 10;
+  }
+
+  return scores;
+}
